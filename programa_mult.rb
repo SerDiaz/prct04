add05ee66331b88805d0dab246da9c0cc2ee3803
GARCIA CAMPOS <alu4394@cc325.(none)>
2013-10-07 18:07:43 +0100

    Matriz c inicializada

--- programa_mult.rb
+++ programa_mult.rb
@@ -3,3 +3,12 @@
 a = [[1, 1, 1], [2, 2, 2], [3, 3, 3]] #matriz a
 b= [[4, 4, 4], [5, 5, 5], [6, 6, 6]] #matriz b
 c = Array.new #matriz c
+
+m, n = a.size, a[1].size
+
+for i in 0...m do
+	c[i] = Array.new
+		for j in 0...n do
+			c[i][j] = 0
+		end	
+end
